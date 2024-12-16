50bc9cde45afbcbeac70542c848efdfebf9360fe
Brian Winkers <winkersbrian@gmail.com>
2024-12-15 17:53:20 -0800

    Add info as default route

--- api/routes/root.js
+++ api/routes/root.js
@@ -1,6 +1,26 @@
 export default async function (fastify, opts) {
-  fastify.get('/',     
+  fastify.get('/', 
+    {
+      schema: {
+        description: 'API Info',
+        tags: ['info'],
+        summary: "Used to provide API meta info to the client",
+        response: {
+          200: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              privacyPolicyUrl: { type: 'string' },
+              termsOfServiceUrl: { type: 'string' }
+            }
+          }
+        }
+      }
+    },   
     async function (request, reply) {
-    return { root: true }
+    return { 
+      privacyPolicyUrl: 'https://www.izzup.com/docs/privacy-policy',
+      termsOfServiceUrl: 'https://www.izzup.com/docs/terms-of-service',
+    }
   })
 }
