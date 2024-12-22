2c51ca750b7007cb913fc7fb30633331ac1b9dba
Brian Winkers <winkersbrian@gmail.com>
2024-12-21 17:24:55 -0800

    Feeds working locally

---
+++ feed/src/composables/izzup.js
@@ -0,0 +1,137 @@
+const useIzzup = () => {
+
+  const fetchFeed = (url) => {
+    console.log(`Fetch ${feed}`)
+  }
+
+  const twtxtToJson = (txt) => {
+
+    const profile = {
+      nick: '',
+      avatar: '',
+      urls: [],
+      description: '',
+      bio: '',
+    }
+    const links = []
+    const employment = []
+    const follows = []
+    const posts = []
+
+    // console.log(txt)
+    const lines = txt.split('\n')
+    // console.log(lines)
+
+    const regexMeta = new RegExp("(^#.*=)")
+    const regexTab = new RegExp("\\t");
+
+    let parts = []
+
+    lines.forEach((line) => {
+      parts = [] // reset on loop
+      //console.log(line)
+      // Check if comment or date.
+      if (regexMeta.test(line)) {
+        // console.log(line)
+        // Trim off first comment character
+        const metaDef = line.slice(1)
+        // Split on '=' 
+        const [n, v] = metaDef.split("=", 2)
+        const name = n.trim()
+        const val = v.trim()
+        // console.log(`${name} => ${val}`)
+        switch (name) {
+          case 'nick':
+            profile.nick = val
+            break
+          case 'avatar':
+            profile.avatar = val
+            break
+          case 'tagline':
+            profile.tagline = val
+            break
+          case 'url':
+            profile.urls.push(val)
+            break
+          case 'feedTitle':
+            profile.feedTitle = val
+            break
+          case 'link':
+            //parts = val.split(' ')
+            // Split val on https
+            let splitAt
+            if (val.indexOf('https://') > 2) {
+              splitAt = val.indexOf('https://')
+            } else if (splitAt < 2) {
+              splitAt = val.indexOf('gemini://')
+            }
+            else if (splitAt < 2) {
+              splitAt = val.indexOf('gopher://')
+            }
+            else if (splitAt < 2) {
+              splitAt = val.indexOf('http://')
+            }
+            links.push([val.substring(0, splitAt).trim(), val.substring(splitAt)])
+            break
+          case 'follow':
+            parts = val.split(' ')
+            follows.push(parts)
+            break
+          // Izzup extensions
+          case 'description':
+            profile.description = val
+            break
+          case 'bio':
+            profile.bio = val
+            break
+          case 'employment':
+            employment.push(val)
+            break
+        }
+
+      }
+
+      if (regexTab.test(line)) {
+        parts = line.split('\t')
+        const postDate = new Date(parts[0])
+        if (postDate.toString() != 'Invalid Date') {
+          posts.push(parts)
+        }
+
+      }
+    })
+    return {
+      profile: profile,
+      links: links,
+      follows: follows,
+      posts: posts
+    }
+  }
+
+  const hashText = async (txt) => {
+    const msgUint8 = new TextEncoder().encode(txt); // encode as (utf-8) Uint8Array
+    const hashBuffer = await window.crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join(""); // convert bytes to hex string
+    return hashHex;
+  }
+  
+
+  const feature = {
+    feedFetching: {
+      enabled: false
+    }
+  }
+
+  return {
+    feature,
+    hashText,
+    fetchFeed,
+    twtxtToJson
+  }
+
+}
+
+export { useIzzup }
---
+++ feed/src/pages/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <template v-if="twtxt">
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item variant="elevated" :title="twtxt.profile.nick"></v-list-item>
+          <v-list-item variant="elevated" :slim="true"><img :src="twtxt.profile.avatar" </v-list-item>
+        </v-list>
+        <v-expansion-panels variant="accordion">
+          <v-expansion-panel>
+            <v-expansion-panel-title>
+              <template v-slot:default="{ expanded }">
+                <v-row no-gutters>
+                  <v-col class="d-flex justify-start" cols="4">
+                    Links
+                  </v-col>
+                </v-row>
+              </template>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-list>
+                <v-list-item v-for="item, ix in twtxt.links" :key="ix">
+                  <a :href="item[1]">{{ item[0] }}</a>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-title>
+              <template v-slot:default="{ expanded }">
+                <v-row no-gutters>
+                  <v-col class="d-flex justify-start" cols="4">
+                    Follows
+                  </v-col>
+                </v-row>
+              </template>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-list>
+                <v-list-item v-for="item, ix in twtxt.follows" :key="ix">
+                  <a :href="`/at/?url=${item[1]}`">{{ item[0] }}</a>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-navigation-drawer>
+
+      <v-app-bar><div class="text-h4 px-5 font-italic">{{ twtxt.profile.tagline }}</div></v-app-bar>
+
+      <v-main class="d-flex justify-center" style="min-height: 600px;">
+        <div class="d-flex flex-column v-col-12 ga-2 pa-4">
+          <v-card v-for="item, ix in twtxt.posts" :key="ix" class="align-end pa-2 rounded w-100">
+            <v-card-subtitle>
+              {{ item[0] }}
+            </v-card-subtitle>
+            <v-card-item>
+              {{ item[1] }}
+            </v-card-item>
+          </v-card>
+        </div>
+      </v-main>
+    </template>
+    <template v-else>
+      Loading...
+    </template>
+  </v-layout>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import { useRoute } from "vue-router"
+
+import { useIzzup } from "../composables/izzup.js"
+
+const izzup = useIzzup()
+
+const route = useRoute()
+
+const member = route.path.split('/')[1]
+const remoteFeed = route.query.url
+
+console.log('member', member)
+console.log('remoteFeed', remoteFeed)
+
+let cacheId
+
+const twtxt = ref()
+
+
+onMounted(async () => {
+
+  if(remoteFeed)  {
+    cacheId = await izzup.hashText(remoteFeed)
+  }
+
+  const twtxtUrl = member ? 'twtxt.txt' : remoteFeed ? `cache/${cacheId}/twtxt.txt` : '/twtxt.txt'
+
+  console.log('twxtxtUrl', twtxtUrl)
+
+
+  let memberTwtxt = false
+  memberTwtxt = await fetch(twtxtUrl)
+  console.log(memberTwtxt)
+  const asText = await memberTwtxt.text()
+  console.log(asText)
+  const asJson = izzup.twtxtToJson(asText)
+  twtxt.value = asJson
+})
+
+</script>
